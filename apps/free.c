a23b008195990fc97b628a17a77c226e43ca96b6
JW <jjwang@ustc.edu>
2026-07-16 22:22:21 +0800

    fix: align free memory table with %-11s %8s format

--- apps/free.c
+++ apps/free.c
@@ -8,11 +8,11 @@ void _start(int argc, char **argv, syscall_t *sys)
     int total_p = sys->get_total_psram();
     int free_p = sys->get_free_psram();
 
-    sys->printf("Memory Type       Total    Used    Free\n");
-    sys->printf("-----------------------------------------\n");
-    sys->printf("Internal       %5dK  %5dK  %5dK\n", total / 1024, (total - free_h) / 1024, free_h / 1024);
+    sys->printf("%-11s %8s %8s %8s\n", "Type", "Total", "Used", "Free");
+    sys->printf("--------------------------------------\n");
+    sys->printf("%-11s %8dK %8dK %8dK\n", "Internal", total / 1024, (total - free_h) / 1024, free_h / 1024);
     if (total_p > 0) {
-        sys->printf("PSRAM          %5dK  %5dK  %5dK\n", total_p / 1024, (total_p - free_p) / 1024, free_p / 1024);
+        sys->printf("%-11s %8dK %8dK %8dK\n", "PSRAM", total_p / 1024, (total_p - free_p) / 1024, free_p / 1024);
     }
     sys->exit(0);
 }
